ea475035d82d7965bc25242111774d64f908a07d
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-16 01:17:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,29 @@
+ class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @grade = grade
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |student_name, grade|
+      @roster[grade] = student_name.sort
+    end
+  end
 
-
-
+end
